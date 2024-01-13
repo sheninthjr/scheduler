f023538ca0f72a14ca44c5161c5b36df54fb4cae
sheninthjr <sheninthjr23@gmail.com>
2024-01-13 14:22:04 +0530

    NavBar fixed

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -37,7 +37,7 @@ const NavBar = () => {
       }
     };
     updateUser();
-  }, [session?.user?.email,userSignin]);
+  }, [session?.user?.email]);
 
   useEffect(() => {
     if (userSignin.data?.userId) {
