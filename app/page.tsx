51966b1b433eca2ad1060d52024332ca5c5e7e67
sheninthjr <sheninthjr23@gmail.com>
2024-01-14 21:12:35 +0530

    Next-auth Redirect is not working

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,6 @@ import { trpc } from "./trpc/client";
 import { useRecoilValue } from "recoil";
 import { userID } from "./store/atoms/userId";
 import Post from "./components/Post";
-import { signIn, useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
 
 interface CheckedItems {
   [key: string]: boolean;
@@ -24,8 +22,6 @@ export default function Home() {
   const { data, refetch } = trpc.schedule.queryPost.useQuery({
     userId: userId.id || "",
   });
-  const router = useRouter()
-  const { data: session } = useSession();
 
   useEffect(() => {
     refetch();
@@ -47,43 +43,29 @@ export default function Home() {
     });
   };
 
-  if(!session){
-    return(
-      <>
-      <div className="h-screen flex justify-center items-center">
-        <div className="bg-white text-black p-2 rounded-lg flex flex-col space-y-4">
-          <div className="text-xl">
-            Welcome to Jr Scheduler
-          </div>
-          <div className="text-red-600">
-            *Please Click the button for Signin
-          </div>
-          <div className="text-white flex justify-center">
-          <button
-                className="flex justify-center items-center w-fit h-8 bg-slate-800 pl-2 pr-2 rounded-lg"
-                onClick={() => signIn()}
-              >
-                Sigin
-              </button>
-          </div>
-        </div>
-      </div>
-      </>
-    )
-  }
+  
   const sortedData = data?.sort((a, b) => {
     const dateA = new Date(a?.day);
     const dateB = new Date(b?.day);
     const today = new Date();
     today.setHours(0, 0, 0, 0);
-    if (dateA.toISOString().substring(0, 10) === today.toISOString().substring(0, 10)) return -1; 
-    if (dateB.toISOString().substring(0, 10) === today.toISOString().substring(0, 10)) return 1;  
-    if (a.done === 'true' && b.done === 'false') return 1;
-    if (a.done === 'false' && b.done === 'true') return -1;
+    if (
+      dateA.toISOString().substring(0, 10) ===
+      today.toISOString().substring(0, 10)
+    )
+      return -1;
+    if (
+      dateB.toISOString().substring(0, 10) ===
+      today.toISOString().substring(0, 10)
+    )
+      return 1;
+    if (a.done === "true" && b.done === "false") return 1;
+    if (a.done === "false" && b.done === "true") return -1;
     if (dateA > dateB) return 1;
     if (dateA < dateB) return -1;
     return 0;
   });
+
   return (
     <>
       <div className="flex justify-center text-white h-screen items-center">
@@ -117,5 +99,4 @@ export default function Home() {
       </div>
     </>
   );
-
 }
